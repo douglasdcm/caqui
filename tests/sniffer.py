a5bb12738ad1ad9ae9d63f2ccf3ae934333c36e2
Douglas Cardoso <douglas.dcm@gmail.com>
2023-05-20 01:29:08 -0300

    remove driver files

---
+++ tests/sniffer.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from pytest import fixture, mark
+from os import getcwd
+
+BASE_DIR = getcwd()
+ROOT_DIR = BASE_DIR + "/caqui/src"
+TEST_DIR = BASE_DIR + "/caqui/tests"
+
+
+@fixture
+def setup():
+    desired_capabilities = {
+        # 'deviceName': 'Device',
+        "deviceName": "Emulator",
+        "deviceIpAddress": "127.0.0.1",
+        "locale": "en-US",
+        "debugCodedUI": False,
+        "app": "chromedriver",
+    }
+
+    driver = webdriver.Remote(
+        command_executor="http://localhost:9999",
+        desired_capabilities=desired_capabilities,
+    )
+    driver.get(f"file:////{TEST_DIR}/playground.html")
+    yield driver
+    driver.quit()
+
+
+def test_get_attribute_from_input(setup):
+    driver = setup
+    search_box = driver.find_element("xpath", "//input")
+    search_box.send_keys("cat")
+
+    # data = driver.find_element("xpath", "//input").get_attribute("value")
+    data = driver.find_element("xpath", "//input").get_property("value")
+    assert data == "cat"
+
+
+@mark.skip("used just to discover request data")
+def test_sniff(setup):
+    driver = setup
+    search_box = driver.find_element("xpath", "//input")
+    search_button = driver.find_element("xpath", "//button")
+    end = driver.find_element("xpath", "//p[@id='end']")
+    search_box.send_keys("cat")
+    search_button.click()
+    assert end.text == "end"
