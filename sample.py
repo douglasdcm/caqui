a92d92e2dc60929a54b69221112d81cef721ae17
Douglas Cardoso <douglas.dcm@gmail.com>
2023-06-02 01:30:34 -0300

    fixing sample and readme

--- sample.py
+++ sample.py
@@ -1,49 +1,44 @@
 import asyncio
 import time
 from caqui import synchronous, asynchronous
-
 from os import getcwd
+from tests.constants import PAGE_URL
 
 BASE_DIR = getcwd()
-ROOT_DIR = BASE_DIR + "/caqui/src"
-TEST_DIR = BASE_DIR + "/tests"
+
+MAX_CONCURRENCY = 5  # number of webdriver instances running
+sem = asyncio.Semaphore(MAX_CONCURRENCY)
 
 
 async def get_all_links():
-    driver_url = "http://127.0.0.1:9999"
-    capabilities = {
-        "desiredCapabilities": {
-            "browserName": "firefox",
-            "marionette": True,
-            "acceptInsecureCerts": True,
+    async with sem:
+        driver_url = "http://127.0.0.1:9999"
+        capabilities = {
+            "desiredCapabilities": {
+                "browserName": "firefox",
+                "marionette": True,
+                "acceptInsecureCerts": True,
+            }
         }
-    }
-    comp_data = [
-        {
-            "page": f"https://www.dqrtech.com.br/vagas/",
-            "locator": f"//a[contains(@title,'Veja detalhes')]",
-        },
-        {
-            "page": f"https://ciandt.com/us/en-us/careers/open-positions",
-            "locator": "//a[contains(@class,'wp-block-cit-block-ciandt-link')]",
-        },
-    ]
 
-    for data in comp_data:
         session = await asynchronous.get_session(driver_url, capabilities)
         await asynchronous.go_to_page(
             driver_url,
             session,
-            data["page"],
+            PAGE_URL,
         )
 
-        locator_type = "xpath"
-        anchors = []
+        for i in range(4):
+            i += 1
+            locator_value = f"//a[@id='a{i}']"
+            locator_type = "xpath"
+            anchors = []
 
-        anchors = await asynchronous.find_elements(
-            driver_url, session, locator_type, data["locator"]
-        )
-        print(f"Found {len(anchors)} links")
+            anchors = await asynchronous.find_elements(
+                driver_url, session, locator_type, locator_value
+            )
+            print(f"Found {len(anchors)} links")
+
         for anchor in anchors:
             text = await asynchronous.get_property(driver_url, session, anchor, "href")
             print(f"Link found '{text}'")
@@ -51,35 +46,20 @@ async def get_all_links():
         synchronous.close_session(driver_url, session)
 
 
+# Reference: https://stackoverflow.com/questions/48483348/how-to-limit-concurrency-with-python-asyncio
 async def main():
-    # Schedule three calls *concurrently*:
-    await asyncio.gather(
-        get_all_links(),
-        get_all_links(),
-        get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-        # get_all_links(),
-    )
+    number_of_websites = range(10)
+    tasks = [asyncio.ensure_future(get_all_links()) for number in number_of_websites]
+    await asyncio.gather(*tasks)
 
 
-start = time.time()
-
-asyncio.run(main())
-
-end = time.time()
-print(f"Time: {end-start:.2f} sec")
+if __name__ == "__main__":
+    start = time.time()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+        end = time.time()
+        print(f"Time: {end-start:.2f} sec")
