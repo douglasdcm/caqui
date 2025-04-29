b194766f3f2bcc485cf94081a61b59f888bad2cd
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-04-28 21:19:54 -0300

    Adding missing files

---
+++ tests/conftest.py
@@ -0,0 +1,71 @@
+from time import sleep
+from caqui.easy import AsyncPage
+from pytest import fixture
+from tests.constants import PAGE_URL
+from caqui.easy.capabilities import ChromeCapabilitiesBuilder
+from caqui.easy.options import ChromeOptionsBuilder
+from caqui.easy.server import Server
+from caqui import synchronous
+from tests.constants import PAGE_URL
+from caqui.easy.server import Server
+
+SERVER_PORT = 9999
+SERVER_URL = f"http://localhost:{SERVER_PORT}"
+CAPTURES="captures"
+def __build_capabilities():
+    options = (
+        ChromeOptionsBuilder()
+        .args(["headless"])
+        .to_dict()
+    )
+    capabilities = (
+        ChromeCapabilitiesBuilder().accept_insecure_certs(True).add_options(options)
+    ).to_dict()
+    return capabilities
+
+@fixture(autouse=True, scope="session")
+def setup_server():
+    server = Server.get_instance(port=SERVER_PORT)
+    server.start()
+    yield
+    sleep(3)
+    server.dispose()
+
+@fixture
+def setup_functional_environment():
+    server_url = SERVER_URL
+    capabilities = __build_capabilities()
+    session = synchronous.get_session(server_url, capabilities)
+    synchronous.go_to_page(
+        server_url,
+        session,
+        PAGE_URL,
+    )
+    yield server_url, session
+    try:
+        synchronous.dismiss_alert(server_url, session)
+    except Exception:
+        pass
+    try:
+        # It is ignored, bacause there are tests theat validate of the windows was closed properlly
+        # Hence, the session is closed previouslly in thise tests
+        synchronous.take_screenshot(SERVER_URL, session, CAPTURES)
+    except Exception:
+        pass
+    try:
+        synchronous.close_session(server_url, session)  
+    except Exception:
+        pass
+
+@fixture
+def setup_environment():
+    server_url = SERVER_URL
+    capabilities =  __build_capabilities()
+    page = AsyncPage(server_url, capabilities, PAGE_URL)
+    yield page
+    try:
+        synchronous.dismiss_alert(server_url, page.session)
+    except Exception:
+        pass
+    synchronous.take_screenshot(SERVER_URL, page.session, CAPTURES)
+    page.quit()
