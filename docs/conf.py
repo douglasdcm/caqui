3857d4d71484fe20f5b36157d77359a83f4c5e21
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2026-02-12 02:05:17 -0300

    Fix version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2025, Douglas Cardoso"
 author = "Douglas Cardoso"
 
 # The full version, including alpha/beta/rc tags
-release = "5.0.1-rc"
+release = "5.0.0rc2"
 
 
 # -- General configuration ---------------------------------------------------
