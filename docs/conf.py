bc3ad1b55802003c9872e4cedda5fabf85a82335
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2026-03-20 22:47:49 -0300

    Bum version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2025, Douglas Cardoso"
 author = "Douglas Cardoso"
 
 # The full version, including alpha/beta/rc tags
-release = "5.0.0"
+release = "6.0.0"
 
 
 # -- General configuration ---------------------------------------------------
